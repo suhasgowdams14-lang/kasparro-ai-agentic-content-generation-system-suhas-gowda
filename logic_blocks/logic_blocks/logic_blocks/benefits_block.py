b70a1ee8c0fc401aa4ecdc0ae63f38e34fe2547d
Suhas Gowda MS <suhasgowdams14@gmail.com>
2025-12-28 00:37:26 +0530

    Delete logic_blocks/logic_blocks directory

--- logic_blocks/logic_blocks/logic_blocks/benefits_block.py
+++
@@ -1,5 +0,0 @@
-def generate_benefits_block(benefits: list) -> list:
-    """
-    Convert a list of benefits into a structured format.
-    """
-    return [{"benefit": benefit} for benefit in benefits]
