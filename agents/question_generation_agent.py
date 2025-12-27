da0917cb6e0689fafc04452b0956ff767c9eb27a
Suhas Gowda MS <suhasgowdams14@gmail.com>
2025-12-28 00:14:59 +0530

    Add QuestionGenerationAgent skeleton

---
+++ agents/question_generation_agent.py
@@ -0,0 +1,8 @@
+class QuestionGenerationAgent:
+    """
+    Responsibility:
+    - Generate categorized user questions based on ProductModel
+    """
+
+    def run(self, product: dict) -> dict:
+        raise NotImplementedError("QuestionGenerationAgent logic not implemented yet")
