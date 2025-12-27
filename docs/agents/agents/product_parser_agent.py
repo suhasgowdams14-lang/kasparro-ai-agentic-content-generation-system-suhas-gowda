4fcccc91415fc56e7252f68d8b31503134f00a1a
Suhas Gowda MS <suhasgowdams14@gmail.com>
2025-12-28 00:06:49 +0530

    Add ProductParserAgent skeleton
    Add ProductParserAgent skeleton

---
+++ docs/agents/agents/product_parser_agent.py
@@ -0,0 +1,9 @@
+class ProductParserAgent:
+    """
+    Responsibility:
+    - Parse raw product input data
+    - Normalize it into the internal ProductModel format
+    """
+
+    def run(self, raw_data: dict) -> dict:
+        raise NotImplementedError("ProductParserAgent logic not implemented yet")
